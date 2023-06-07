e40711adf815d4699fdd8b3c5cc9a0e4a6bce01c
OverlordCrowley <64090766+OverlordCrowley@users.noreply.github.com>
2023-06-07 15:02:22 +0600

    aa

--- SliceDelivery/Controllers/HomeController.cs
+++ SliceDelivery/Controllers/HomeController.cs
@@ -47,10 +47,6 @@ namespace SliceDelivery.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
