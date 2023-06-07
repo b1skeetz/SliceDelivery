21fa5eec2670ba12e062ae1ca4c5a786cb79d614
OverlordCrowley <64090766+OverlordCrowley@users.noreply.github.com>
2023-06-07 14:00:32 +0600

    pizdec

---
+++ SliceDelivery/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SliceDelivery.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SliceDelivery.Controllers
+{
+    public class AboutController : Controller
+    {
+    
+
+      
+        public IActionResult Index()
+        {
+            return View("About");
+        }
+
+     
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
